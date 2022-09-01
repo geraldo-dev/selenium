c8a3a2fc7d35acf1b58640880ea1f9bd3df94be0
geraldo sousa <geraldosousadev@gmail.com>
2022-09-01 09:28:16 -0300

    removi folde driver_Notes and  picking up search field

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+from lib2to3.pgen2 import driver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 brownser = webdriver.Edge()
 
-brownser.get('https://www.instagram.com/geraldo_fnt/')
+brownser.get('https://www.youtube.com/')
+
+tag = 'search_query'
+elem = brownser.find_element(By.NAME, tag)
+
+elem.send_keys('geraldo testando algo kkk')
+sleep(2)
+elem.send_keys(Keys.RETURN)
+
 
 sleep(3)
 
