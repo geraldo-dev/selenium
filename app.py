058b4b24d2f5c38d98178edec1d06c71a7ff0644
geraldo sousa <geraldosousadev@gmail.com>
2022-09-07 19:31:09 -0300

    restructuring code

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-brownser = webdriver.Edge()
+drive = webdriver.Edge()
 
-brownser.get('https://www.youtube.com/')
+drive.get('https://www.mercadolivre.com.br/')
 
-search_query = brownser.find_element(By.NAME, 'search_query')
-search_query.send_keys('metallica seek and destroy')
-search_query.send_keys(Keys.RETURN)
-
-sleep(1)
-lnk = brownser.find_element(By.LINK_TEXT, 'Metallica - Seek And Destroy (HD)')
-lnk.click()
-
-#like
 sleep(3)
-brownser.close()
+
+drive.close()
