437292a834fcb45606660bf25ce12ddeb706112a
geraldo sousa <geraldosousadev@gmail.com>
2022-09-07 20:07:20 -0300

    picking up card information

--- app.py
+++ app.py
@@ -6,10 +6,22 @@ drive = webdriver.Edge()
 
 drive.get('https://www.mercadolivre.com.br/')
 
+
 search_name = drive.find_element(By.NAME, 'as_word')
 search_name.send_keys('notebook')
 
 search_btn = drive.find_element(By.CLASS_NAME, 'nav-search-btn').click()
+
+sleep(2)
+cards = drive.find_elements(By.CLASS_NAME, 'ui-search-result__wrapper')
+price = 'price-tag-fraction'
+freete = 'ui-search-item__shipping--free'
+description = 'ui-search-item__title'
+
+for card in cards:
+    print('price: ', card.find_element(By.CLASS_NAME, price).text)
+    print('freete: ', card.find_element(By.CLASS_NAME, freete).text)
+    print('description: ', card.find_element(By.CLASS_NAME, description).text)
 
 sleep(5)
 
