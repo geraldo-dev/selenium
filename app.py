24b1a8e742d8a2707d77b899c361ddb0660024c5
geraldo sousa <geraldosousadev@gmail.com>
2022-09-07 19:42:00 -0300

    capture search fields and search button

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ drive = webdriver.Edge()
 
 drive.get('https://www.mercadolivre.com.br/')
 
-sleep(3)
+search_name = drive.find_element(By.NAME, 'as_word')
+search_name.send_keys('notebook')
+
+search_btn = drive.find_element(By.CLASS_NAME, 'nav-search-btn').click()
+
+sleep(5)
 
 drive.close()
