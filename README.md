e782d4baed603ea8d65fd80d72a133c2ff9d7bce
geraldo sousa <geraldosousadev@gmail.com>
2022-09-07 20:40:14 -0300

    Instructions

---
+++ README.md
@@ -0,0 +1,17 @@
+## create virtual environment
+````
+python -m venv env
+````
+## install dependencies
+````
+pip install -r requirements.txt
+````
+## activate virtual environment
+in the window
+````
+env\scripts\active.bat
+````
+## start project
+````
+flask run
+````
--- app.py
+++ app.py
@@ -6,25 +6,22 @@ drive = webdriver.Edge()
 
 drive.get('https://www.mercadolivre.com.br/')
 
+datas = []
 
 search_name = drive.find_element(By.NAME, 'as_word')
 search_name.send_keys('notebook')
 
 search_btn = drive.find_element(By.CLASS_NAME, 'nav-search-btn').click()
 
-sleep(2)
-cards = drive.find_elements(By.CLASS_NAME, 'ui-search-result__wrapper')
+boxs = drive.find_elements(By.CLASS_NAME, 'ui-search-result__wrapper')
 
-datas = []
 
-for card in cards:
+for box in boxs:
     datas.append({
-        'price' : card.find_element(By.CLASS_NAME, 'price-tag-fraction').text,
-        'frete': card.find_element(By.TAG_NAME, 'p').text,
-        'description': card.find_element(By.TAG_NAME, 'h2').text
+        'price' : box.find_element(By.CLASS_NAME, 'price-tag-fraction').text,
+        'frete': box.find_element(By.TAG_NAME, 'p').text,
+        'description': box.find_element(By.TAG_NAME, 'h2').text
     })
-
-sleep(2)
 
 print(datas)
 
